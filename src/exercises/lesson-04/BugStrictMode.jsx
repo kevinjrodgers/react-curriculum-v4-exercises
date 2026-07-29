1d1051a94f68748d457e93f959d94fe711eeae84
unknown <kevjrodgers@gmail.com>
2026-07-28 17:56:49 -0700

    Completed Week 4 exercises

--- src/exercises/lesson-04/BugStrictMode.jsx
+++ src/exercises/lesson-04/BugStrictMode.jsx
@@ -7,9 +7,10 @@ export default function BugStrictMode() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setInterval(() => {
+    const secondsTimer = setInterval(() => {
       setCount((c) => c + 1);
     }, 1000);
+    return () => clearInterval(secondsTimer);
   }, []);
 
   return (
@@ -21,3 +22,10 @@ export default function BugStrictMode() {
 }
 
 // Write your explanation of how StrictMode helps us catch this bug
+/*
+  StrictMode mounts -> unmounts -> mounts again when in developer mode. Due to StrictMode, we can properly see that it increments by 2, so
+  there was a bug due to the counter not being cleared each time. The setInterval() method will mount and then unmounts, and on the final
+  mount it will increment by 1. Initially, the setInterval() count was not cleared. So when the setInterval() method was mounted, it incremented
+   the count by 1. Then it was unmounted. On the re-mount, because the interval wasn't cleared, it ran again, and now it increments by 2. By 
+   assigning the method to a variable, we can now target it in order to clear it so it only runs once instead of twice during StrictMode. 
+*/
--- src/exercises/lesson-04/FindCorrectHook.jsx
+++ src/exercises/lesson-04/FindCorrectHook.jsx
@@ -1,16 +1,22 @@
 // TOPIC: Choose the correct tool: useRef vs useState
 // TASK: Make sure it updates the text *without* triggering a re-render
+import { useRef } from 'react';
+
 export default function FindCorrectHook() {
-  let clickCount = 0; // ← incorrect implementation
+  let clickCount = useRef(0); // ← incorrect implementation
+  let buttonRef = useRef();
 
   function handleClick() {
-    clickCount++;
+    clickCount.current = clickCount.current + 1;
+    buttonRef.current.innerHTML = clickCount.current + ' Clicks';
   }
 
   return (
     <div>
       <h2>useRef vs useState Decision</h2>
-      <button onClick={handleClick}>{clickCount} Clicks</button>
+      <button onClick={handleClick} ref={buttonRef}>
+        0 Clicks
+      </button>
     </div>
   );
 }
