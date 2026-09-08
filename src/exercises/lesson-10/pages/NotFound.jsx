e7312a92fcfde2bb4b92c6b1bc1279216afeac0c
unknown <kevjrodgers@gmail.com>
2026-09-08 14:15:03 -0700

    Completed Exercise 4B

--- src/exercises/lesson-10/pages/NotFound.jsx
+++ src/exercises/lesson-10/pages/NotFound.jsx
@@ -1,11 +1,13 @@
 import { Link, useLocation } from 'react-router-dom';
 
 export default function NotFound() {
-  const pathname = '';
+  const { pathname } = useLocation();
 
   return (
     <section>
       <h2>404: Not Found</h2>
+      <p>{pathname} is invalid</p>
+      <Link to="/lessons/lesson-10/">Go back to Home</Link>
     </section>
   );
 }
